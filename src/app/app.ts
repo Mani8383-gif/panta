fbfdf1a6d85870aed7570ddb72b0f87721d1c4aa
Mani <manihoshiaripour83@gamil.com>
2025-11-07 18:37:47 +0330

    save my current work before ng update

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from "./navbar/navbar";
+import { Footer } from "./footer/footer";
+import { Photo } from "./photo/photo";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, Footer, Photo],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
